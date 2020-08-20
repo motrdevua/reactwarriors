14aa0b47db96c13a03a9a1c7faec9ed50b2aef93
Igor Lukashev <48984371+motrdevua@users.noreply.github.com>
2020-08-20 15:51:01 +0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,109 @@
+import React from 'react';
+import './styles.css';
+
+import { moviesData } from './moviesData';
+import MovieItem from './MovieItem';
+
+const API_URL = 'https://api.themoviedb.org/3';
+
+const API_KEY_3 = '3f4ca4f3a9750da53450646ced312397';
+
+// UI: state, props
+
+class App extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      movies: moviesData,
+      moviesWillWatch: []
+    };
+
+    this.removeMovie = this.removeMovie.bind(this);
+    this.addMovieToWillWatch = this.addMovieToWillWatch.bind(this);
+  }
+
+  removeMovie(movie) {
+    const updateMovies = this.state.movies.filter((item) => {
+      return item.id !== movie.id;
+    });
+    this.setState({
+      movies: updateMovies
+    });
+  }
+
+  addMovieToWillWatch(movie) {
+    // const updateMoviesWillWatch = [...this.state.moviesWillWatch];
+    // updateMoviesWillWatch.push(movie);
+
+    const updateMoviesWillWatch = [...this.state.moviesWillWatch, movie];
+    this.setState({
+      moviesWillWatch: updateMoviesWillWatch
+    });
+  }
+
+  removeMovieFromWillWatch(movie) {
+    console.log(this.state);
+
+    const updateMoviesWillWatch = this.state.moviesWillWatch.filter((item) => {
+      return item.id !== movie.id;
+    });
+    this.setState({
+      moviesWillWatch: updateMoviesWillWatch
+    });
+  }
+
+  componentDidMount() {
+    console.log('didMount');
+
+    fetch(`${API_URL}/discover/movie?api_key=${API_KEY_3}`)
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        this.setState({
+          movies: data.results
+        });
+      });
+  }
+
+  render() {
+    return (
+      <div className="container">
+        <div className="row">
+          <div className="col-9" style={{ background: '#ccc' }}>
+            <div className="container  p-0">
+              <div className="row p-0">
+                {this.state.movies.map((movie) => {
+                  return (
+                    <MovieItem
+                      key={movie.id}
+                      movie={movie}
+                      removeMovie={this.removeMovie}
+                      addMovieToWillWatch={this.addMovieToWillWatch}
+                      removeMovieFromWillWatch={this.removeMovieFromWillWatch}
+                    />
+                  );
+                })}
+              </div>
+            </div>
+          </div>
+          <div
+            className="col-3 pt-4"
+            style={{ position: 'relative', background: '#aaa' }}>
+            <div
+              className="d-flex justify-content-center align-items-center"
+              style={{ position: 'fixed' }}>
+              <p>Will watch:</p>
+              <p className="pl-3 text-primary">
+                {this.state.moviesWillWatch.length}
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/MovieItem.jsx
@@ -0,0 +1,74 @@
+import React from 'react';
+
+class MovieItem extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      willWatch: false
+    };
+  }
+
+  render() {
+    const {
+      movie,
+      removeMovie,
+      addMovieToWillWatch,
+      removeMovieFromWillWatch
+    } = this.props;
+
+    return (
+      <div className="col-lg-6 col-md-6 col-sm-6 col-12 mt-3 ">
+        <div className="card">
+          <p className="card__title mb-0">{movie.title}</p>
+          <img
+            src={`https://image.tmdb.org/t/p/w500/${
+              movie.poster_path || movie.backdrop_path
+            }`}
+            alt={movie.title}
+          />
+          <p className="d-flex align-items-center justify-content-center rate p-2 m-0">
+            Rating: {movie.vote_average}
+          </p>
+          <div
+            style={{ width: '100%' }}
+            className="p-1 d-flex justify-content-between align-items-center">
+            {this.state.willWatch ? (
+              <button
+                type="button"
+                className="btn btn-secondary p-1"
+                onClick={() => {
+                  removeMovieFromWillWatch(movie);
+                  this.setState({
+                    willWatch: false
+                  });
+                }}>
+                will watch remove
+              </button>
+            ) : (
+              <button
+                type="button"
+                className="btn btn-primary p-1"
+                onClick={() => {
+                  addMovieToWillWatch(movie);
+                  this.setState({
+                    willWatch: true
+                  });
+                }}>
+                will watch add
+              </button>
+            )}
+            <button
+              type="button"
+              className="btn btn-warning p-1"
+              onClick={removeMovie.bind(this, movie)}>
+              delete
+            </button>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default MovieItem;
