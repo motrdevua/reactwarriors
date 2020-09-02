7cfee72fec608346a0f22a12936176257bf2257d
Igor Lukashev <motrdevua@gmail.com>
2020-09-02 22:40:22 +0300

    Updates

---
+++ precache-manifest.c58769397fa62024c6827ecd7ab3fa76.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c86251d07668fcad94f0156225d342b4",
+    "url": "/reactwarriors/index.html"
+  },
+  {
+    "revision": "7ef85ab23c938f1102a6",
+    "url": "/reactwarriors/static/css/2.c11e63b2.chunk.css"
+  },
+  {
+    "revision": "3f673fdce2cf9d7ef8f9",
+    "url": "/reactwarriors/static/css/main.77d8a889.chunk.css"
+  },
+  {
+    "revision": "7ef85ab23c938f1102a6",
+    "url": "/reactwarriors/static/js/2.bbe05cdd.chunk.js"
+  },
+  {
+    "revision": "3f673fdce2cf9d7ef8f9",
+    "url": "/reactwarriors/static/js/main.dd84f76b.chunk.js"
+  },
+  {
+    "revision": "74bfa0b95bb4d69c2c4a",
+    "url": "/reactwarriors/static/js/runtime~main.b2445f09.js"
+  }
+]);
